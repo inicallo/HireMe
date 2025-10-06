b40a307b45cde89c8042a8bc8946e6e0a4dd19a2
Calvin Angelo <angelo_calvin@yahoo.co.id>
2025-10-06 14:04:54 +0800

    feat: revise AppClass index

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,8 @@
-const { AppClass } = require('./AppClass'); 
+const AppClass = require('./AppClass'); 
 const { connectDB } = require('./utils/connectDB');
 
 connectDB();
 
-const app = new AppClass();
+const app = new AppClass(); 
+
 module.exports = app.getApp();
